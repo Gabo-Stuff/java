241cd89c26007465c2e41cb266baebc6e3c0849f
Gabriel <sneivergm@gmail.com>
2025-05-12 13:52:25 +0200

    fizzbuzz final final

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class FizzBuzz {
 
     public static void fizzBuzz(int n) {
-        System.out.print(n);
-        for (int i=0;i==n;i++) {
-            System.out.print("Fizz 2");
-            if(n%3==0) {
-                System.out.print("Fizz ");
-            } else if(n%5==0) {
-                System.out.print("Buzz ");
-            }  else if (n%3==0 && n%5==0) {
-                System.out.print("FizzBuzz ");
-            } else System.out.print(n + " ");
+        for (int i=1;i<=n;i++) {
+            if (i%3==0 && i%5==0) {
+                System.out.print(",FizzBuzz");
+            } else if(i%3==0) {
+                System.out.print(",Fizz");
+            } else if(i%5==0) {
+                System.out.print(",Buzz");
+            } else System.out.print(","+i);
         }
     }
 
@@ -21,7 +19,9 @@ public class FizzBuzz {
         System.out.println("Añade un número; a ver ese FizzBuzz:");
         int num1 = myObj.nextInt();
         myObj.close();
+
         
+        System.out.println("");
         fizzBuzz(num1);
     }
 }
