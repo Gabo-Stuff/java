114d5051fd324525b07ec95b5e1f21a0f84b276a
Gabriel <sneivergm@gmail.com>
2025-05-12 13:42:44 +0200

    test commit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class FizzBuzz {
 
     public static void fizzBuzz(int n) {
-        for (int i=0;i==n;i+=1) {
+        System.out.print(n);
+        for (int i=0;i==n;i++) {
+            System.out.print("Fizz 2");
             if(n%3==0) {
                 System.out.print("Fizz ");
             } else if(n%5==0) {
@@ -18,9 +20,9 @@ public class FizzBuzz {
         Scanner myObj = new Scanner(System.in); 
         System.out.println("Añade un número; a ver ese FizzBuzz:");
         int num1 = myObj.nextInt();
-        System.out.println(3%3==0);
-        fizzBuzz(num1);
         myObj.close();
+        
+        fizzBuzz(num1);
     }
 }
 
