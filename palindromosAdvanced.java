9a89baf177b290e7d0536cc24753aa8b51d817ba
Gabriel <sneivergm@gmail.com>
2025-05-19 10:56:37 +0200

    test commit

---
+++ palindromosAdvanced.java
@@ -0,0 +1,40 @@
+package org.gaboCompany.myproject;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class palindromosAdvanced {
+
+    private static String palindromos(String word) {
+        String pali = "";
+        word = StringUtils.stripAccents(word).toLowerCase().replaceAll(" ", "").replaceAll("\\p{Punct}", "");
+        for (int i = 0; i < word.length(); i++) {
+            char x = word.charAt(i);
+            pali = x + pali;
+        }
+        return pali;
+    }
+
+    private static boolean assertEquals(String exp, String act) {
+        System.out.println(exp+" = "+act);
+        return exp.equals(act);
+    }
+
+    private static void testPalindromos() {
+        if (assertEquals(palindromos("rapÁr!?.;."), "rapar")) {
+            System.out.println("bieeen");
+        } else System.err.println("Noo");
+        
+        if (assertEquals(palindromos("pÂn!.,;"), "pan")) {
+            System.out.println("bieeen");
+        } else System.err.println("Noo");
+
+        if (assertEquals(palindromos("La ruta natural!??.;"), "larutanatural")) {
+            System.out.println("bieeen");
+        } else System.err.println("Noo");
+    }
+    
+    
+    public static void main(String args[]) {
+        testPalindromos();
+    }
+}
